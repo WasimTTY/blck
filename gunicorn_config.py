c4fbdc3dabfd0a393075be28f2f3c05c5281d90d
Mohamed Wasim <wasimmdmohamed@gmail.com>
2025-01-23 13:13:15 +0530

    chore: add logging configuration to Gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,13 +1,11 @@
 import os
-
 workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
-
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
-
 forwarded_allow_ips = '*'
-
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+project_root = os.path.dirname(os.path.abspath(__file__))  
+accesslog = os.path.join(project_root, 'access.log')  
+errorlog = os.path.join(project_root, 'error.log')  
+loglevel = os.environ.get('GUNICORN_LOG_LEVEL', 'debug')  
